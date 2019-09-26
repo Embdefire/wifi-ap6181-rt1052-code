e540eb34c970a6b607fee02c3e907be107a917f9
fbc <849067797@qq.com>
2019-09-26 15:50:24 +0800

    wifi scan ok

---
+++ ping/user/main.c
@@ -0,0 +1,173 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2019-xx-xx
+  * @brief   i.MX RT + FreeRTOS + LWIP + AP6181（wifi）
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火  i.MXRT1052开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************
+  */
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "lwip/opt.h"
+#include "lwip/icmp.h"
+#include "lwip/inet_chksum.h"
+#include "lwip/sockets.h"
+#include "lwip/mem.h"
+#include "lwip/inet.h"
+#include "netif/etharp.h"
+#include "lwip/tcpip.h"
+#include "lwip/dhcp.h"
+#include "lwip/prot/dhcp.h"
+#include "wwd_network.h"
+#include "wwd_management.h"
+#include "wwd_wifi.h"
+#include "wwd_debug.h"
+#include "wwd_assert.h"
+#include "platform/wwd_platform_interface.h"
+#include "RTOS/wwd_rtos_interface.h"
+#include "wwd_buffer_interface.h"
+
+#include "board.h"
+
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "fsl_gpio.h"
+#include "fsl_iomuxc.h"
+#include "./led/bsp_led.h" 
+
+#include "ping.h"
+
+
+/** @endcond */
+
+/*****************************************************
+ *               静态函数声明
+ ******************************************************/
+
+static void tcpip_init_done( void * arg );
+static void startup_thread( void *arg );
+
+/******************************************************
+ *               变量定义
+ ******************************************************/
+
+static TaskHandle_t  startup_thread_handle;
+
+/******************************************************
+ *               功能定义
+ ******************************************************/
+
+static void BOARD_USDHCClockConfiguration(void)
+{
+    /*将系统pll PFD2分数分频器配置为18*/
+    CLOCK_InitSysPfd(kCLOCK_Pfd0, 0x12U);
+    /* 配置USDHC时钟源和分频器*/
+    CLOCK_SetDiv(kCLOCK_Usdhc2Div, 0U);
+    CLOCK_SetMux(kCLOCK_Usdhc2Mux, 1U);
+}
+
+/**
+ * 主函数
+ */
+
+int main( void )
+{
+    gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
+
+    /* 初始化内存管理单元 */
+    BOARD_ConfigMPU();
+    /* 初始化开发板引脚 */
+    BOARD_InitPins();
+    /* 初始化开发板时钟 */
+    BOARD_BootClockRUN();
+    /* 初始化SDIO时钟 */
+    BOARD_USDHCClockConfiguration();
+    /* 初始化调试串口 */
+    BOARD_InitDebugConsole();
+    /* 初始化LED */
+    LED_GPIO_Config();
+    /* 设置SDIO中断优先级 */
+    NVIC_SetPriority(USDHC2_IRQn, 5U);
+
+    IOMUXC_EnableMode(IOMUXC_GPR, kIOMUXC_GPR_ENET1TxClkOutputDir, true);
+    /* 初始化PHY RESET引脚 */
+    GPIO_PinInit(GPIO1, 9, &gpio_config);
+    /* 初始化PHY INT引脚 */
+    GPIO_PinInit(GPIO1, 10, &gpio_config);
+    /* 在复位之前ENET_INT输出高电平 */
+    GPIO_WritePinOutput(GPIO1, 10, 1);
+    GPIO_WritePinOutput(GPIO1, 9, 0);
+    GPIO_WritePinOutput(GPIO1, 9, 1);
+    /*创建一个初始线程 */
+    xTaskCreate(startup_thread, "app_thread", APP_THREAD_STACKSIZE/sizeof( portSTACK_TYPE ), NULL, DEFAULT_THREAD_PRIO, &startup_thread_handle);
+
+    /* 启动FreeRTOS调度程序-此调用永远不会返回*/
+    vTaskStartScheduler( );
+
+    /* 除非vTaskStartScheduler中出现错误，否则永远不要到这里 */
+    WPRINT_APP_ERROR(("Main() function returned - error" ));
+    return 0;
+}
+
+
+
+/**
+ *  初始线程功能-启动LwIP并调用app_main
+ * 该函数使用tcpip_init函数启动LwIP，然后等待信号量，
+ * 直到LwIP通过调用回调@ref tcpip_init_done指示其已
+ * 启动。完成后，将调用应用程序的@ref app_main函数。
+ * @param arg :  未使用-符合线程功能原型所需
+ */
+
+static void startup_thread( void *arg )
+{
+    SemaphoreHandle_t lwip_done_sema;
+    UNUSED_PARAMETER( arg);
+
+    WPRINT_APP_INFO( ( "\nPlatform " PLATFORM " initialised\n" ) );
+    WPRINT_APP_INFO( ( "Started FreeRTOS" FreeRTOS_VERSION "\n" ) );
+    WPRINT_APP_INFO( ( "Starting LwIP " LwIP_VERSION "\n" ) );
+
+    /* 创建信号量以在LwIP完成初始化时发出信号 */
+    lwip_done_sema = xSemaphoreCreateCounting( 1, 0 );
+    if ( lwip_done_sema == NULL )
+    {
+        /*无法创建信号量 */
+        WPRINT_APP_ERROR(("Could not create LwIP init semaphore"));
+        return;
+    }
+
+    /*初始化LwIP，提供回调函数和回调信号量 */
+    tcpip_init( tcpip_init_done, (void*) &lwip_done_sema );
+    xSemaphoreTake( lwip_done_sema, portMAX_DELAY );
+    vQueueDelete( lwip_done_sema );
+
+    /* 运行主应用程序功能 */
+    app_main( );
+
+    /* 清理此启动线程*/
+    vTaskDelete( startup_thread_handle );
+}
+
+
+
+/**
+ *  LwIP初始化完成回调
+ * @param arg : the handle for the semaphore to post (cast to a void pointer)
+ */
+
+static void tcpip_init_done( void * arg )
+{
+    SemaphoreHandle_t * lwip_done_sema = (SemaphoreHandle_t *) arg;
+    xSemaphoreGive( *lwip_done_sema );
+}
+
