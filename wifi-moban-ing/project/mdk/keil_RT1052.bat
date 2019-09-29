87ddf2a41120ebde19b9003eab684a8849fe1643
fbc <849067797@qq.com>
2019-09-29 20:30:01 +0800

    增加 工程模板 但是 只有 nor_tat_ram可以用

---
+++ wifi-moban-ing/project/mdk/keil_RT1052.bat
@@ -0,0 +1,10 @@
+rd  /s /q  ictm_txt_ram
+rd  /s /q  ictm_txt_sdram
+rd  /s /q  nor_itcm_txt_ram
+rd  /s /q  nor_sdram_txt_sdram
+rd  /s /q  nor_txt_ram
+rd  /s /q  nor_txt_sdram
+rd  /s /q  sdram_txt_ram
+
+
+exit
